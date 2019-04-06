49a9df7426f97c0245b4ad104bd9428a0a67ba99
tfchan <cseelemon@gmail.com>
2019-04-06 16:33:58 +0800

    Add argument parser
    Add argument parser for Gaussian data generator

--- hw3-1a.py
+++ hw3-1a.py
@@ -1,9 +1,16 @@
 #!python3
 """Univariate Gaussian data generator."""
+from argparse import ArgumentParser
 
 
 def main():
     """Perform main task of the program."""
+    parser = ArgumentParser(description='Univariate Gaussian data generator')
+    parser.add_argument('mean', type=float, help='Gaussian mean')
+    parser.add_argument('variance', type=float, help='Gaussian variance')
+    parser.add_argument('-n', '--n_data', type=int, default=1,
+                        help='Number of data to generate')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
