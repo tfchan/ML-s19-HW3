e8d60f4e0367395609e05a047f966eb068753e09
tfchan <cseelemon@gmail.com>
2019-04-10 21:05:21 +0800

    Complete add_sample function in regressor class
    Finish calculating posterior mean and covariance after a sample has been
    added to regressor

--- hw3_3.py
+++ hw3_3.py
@@ -1,6 +1,8 @@
 #!python3
 """Baysian Linear regression."""
 from argparse import ArgumentParser
+import numpy as np
+# import hw3_1b
 
 
 class BaysianLinearRegressor():
@@ -11,11 +13,34 @@ class BaysianLinearRegressor():
         self._n_sample = 0
         self._n_basis = n_basis
         self._noise_var = noise_var
-        self._prior = prior
-        self._posterior = None
+        self._noise_para = 1 / noise_var
+        self._prior_m = np.zeros((n_basis, 1))
+        self._prior_cv = prior * np.identity(n_basis)
+        self._posterior_m = None
+        self._posterior_cv = None
 
     def add_sample(self, x, y):
         """Add a sample (x,y) for training."""
+        x_basis = np.array([x**i for i in range(self._n_basis)])
+        prior_cv_inv = np.linalg.inv(self._prior_cv)
+        self._posterior_cv = np.linalg.inv(prior_cv_inv
+                                           + (self._noise_para
+                                              * np.outer(x_basis, x_basis)))
+        self._posterior_m = (self._posterior_cv
+                             @ (prior_cv_inv @ self._prior_m
+                                + self._noise_para * np.outer(x_basis, y)))
+        self._prior_cv = self._posterior_cv
+        self._prior_m = self._posterior_m
+
+    @property
+    def mean(self):
+        """Getter of posterior mean."""
+        return self._posterior_m
+
+    @property
+    def convariance(self):
+        """Getter of posterior covariance."""
+        return self._posterior_cv
 
 
 def main():
@@ -30,6 +55,11 @@ def main():
     args = parser.parse_args()
 
     regressor = BaysianLinearRegressor(len(args.w), args.a, args.b)
+    regressor.add_sample(-0.64152, 0.19039)
+    regressor.add_sample(0.07122, 1.63175)
+    regressor.add_sample(-0.19330, 0.24507)
+    # data_point = hw3_1b.polynomial(args.a, args.w)
+    # regressor.add_sample(*hw3_1b.polynomial(args.a, args.w))
 
 
 if __name__ == '__main__':
